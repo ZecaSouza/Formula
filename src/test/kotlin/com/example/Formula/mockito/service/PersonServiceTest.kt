136da4e0fa2e01ff7c3cf21aa418416f396803e4
Seu Nome <seu@email.com>
2025-07-08 16:11:47 -0300

    test: add unit tests for PersonService methods (create, update, delete, findAll)

--- src/test/kotlin/com/example/Formula/mockito/service/PersonServiceTest.kt
+++ src/test/kotlin/com/example/Formula/mockito/service/PersonServiceTest.kt
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.Mockito.verify
 import java.util.Optional
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -54,22 +56,95 @@ class PersonServiceTest {
         assertEquals("FEMALE", result.gender)
     }
 
-
-
     @Test
     fun create() {
+        val personId = 1L
+        val vo = inputObject.mockVO(1).apply { key = personId }
+
+        val entity = inputObject.mockEntity(1).apply { id = personId }
+
+        `when`(repository.save(any())).thenReturn(entity)
+
+        val result = service.create(vo)
+
+        assertNotNull(result)
+        assertEquals(personId, result.key)
+        assertTrue(result.links.any { it.rel.value() == "self" })
+        assertEquals("Addres Test1", result.address)
+        assertEquals("First Name Test1", result.firstName)
+        assertEquals("Last name 1", result.lastName)
+        assertEquals("FEMALE", result.gender)
     }
 
     @Test
     fun updateById() {
+        val personId = 1L
+        val vo = inputObject.mockVO(1).apply { key = personId }
+
+        val entity = inputObject.mockEntity(1).apply { id = personId }
+
+        `when`(repository.findById(personId)).thenReturn(Optional.of(entity))
+        `when`(repository.save(any())).thenReturn(entity)
+
+        val result = service.updateById(personId, vo)
+
+        assertNotNull(result)
+        assertEquals(personId, result.key)
+        assertTrue(result.links.any { it.rel.value() == "self" })
+        assertEquals("Addres Test1", result.address)
+        assertEquals("First Name Test1", result.firstName)
+        assertEquals("Last name 1", result.lastName)
+        assertEquals("FEMALE", result.gender)
+
     }
 
     @Test
     fun deleteById() {
+        val id = 1L
+        `when`(repository.existsById(id)).thenReturn(true)
+
+        service.deleteById(id)
+
+        verify(repository).deleteById(id)
     }
 
     @Test
     fun findAll() {
+        val person = inputObject.mockEntity(1)
+        val person2 = inputObject.mockEntity(2).apply {
+            id = 2
+            address = "Different Address"
+            firstName = "Different First Name"
+            lastName = "Different Last Name"
+            gender = "MALE"
+        }
+
+        val personList = listOf(person, person2)
+
+        `when`(repository.findAll()).thenReturn(personList)
+
+        val result = service.findAll()
+
+        assertNotNull(result)
+        assertEquals(2, result.size, "Deve retornar 2 itens")
+
+        // Verifica o primeiro item
+        val firstPerson = result[0]
+        assertEquals(1L, firstPerson.key)
+        assertTrue(firstPerson.links.any { it.rel.value() == "self" }, "Deve ter link 'self'")
+        assertEquals("Addres Test1", firstPerson.address)
+        assertEquals("First Name Test1", firstPerson.firstName)
+        assertEquals("Last name 1", firstPerson.lastName)
+        assertEquals("FEMALE", firstPerson.gender)
+
+        // Verifica o segundo item
+        val secondPerson = result[1]
+        assertEquals(2L, secondPerson.key)
+        assertTrue(secondPerson.links.any { it.rel.value() == "self" }, "Deve ter link 'self'")
+        assertEquals("Different Address", secondPerson.address)
+        assertEquals("Different First Name", secondPerson.firstName)
+        assertEquals("Different Last Name", secondPerson.lastName)
+        assertEquals("MALE", secondPerson.gender)
     }
 
 }
